45e90bf3bdd46e039254f50975af151eea587a7b
Elen <elenirazabal@gmail.com>
2020-06-05 01:27:51 +0200

    primer commit a paquete_thebridge

---
+++ funciones/function.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def funcion_a(num):
+    total = np.array([num, 30]) + 6
+    print('casi acabamos')
+    return total
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='funciones',
+    author = 'elen_irazabal',
+    packages = ['funciones'],
+    version = '0.0.1',
+    description = 'Paquete con un ejemplo de la estructura que debe tener un paquete para instalarlo con pip',
+    install_requires = ['numpy']
+)
